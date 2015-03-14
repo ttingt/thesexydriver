822de3801ee3ba76da780052ac6bb840a5ac0d86
Ting Ting Tai <tting.git@gmail.com>
2015-03-14 11:41:46 -0700

    linked up summary page with ratins

--- app/src/main/java/com/costbear/android/thesexydriver/SummaryActivity.java
+++ app/src/main/java/com/costbear/android/thesexydriver/SummaryActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class SummaryActivity extends ActionBarActivity {
@@ -13,6 +14,11 @@ public class SummaryActivity extends ActionBarActivity {
     int brakeRatingSoFar;
     int speedPtsCount;
     int speedRatingSoFar;
+
+    TextView brakingRatingTextView;
+    TextView ratingTextView;
+    TextView speedRatingTextView;
+    TextView ratingMsgTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,16 @@ public class SummaryActivity extends ActionBarActivity {
         brakeRatingSoFar = i.getIntExtra("brakeRatingSoFar", 0);
         speedPtsCount = i.getIntExtra("speedPtsCount", 0);
         speedRatingSoFar = i.getIntExtra("speedRatingSoFar", 0);
+
+        brakingRatingTextView = (TextView) findViewById(R.id.brakingrating);
+        ratingTextView = (TextView) findViewById(R.id.rating);
+        ratingMsgTextView = (TextView) findViewById(R.id.ratingmsg);
+
+        brakingRatingTextView.setText(String.valueOf(brakeRatingSoFar/brakePtsCount));
+        speedRatingTextView.setText(String.valueOf(speedRatingSoFar/speedPtsCount));
+        double overallRating = brakeRatingSoFar/brakePtsCount*.4 + speedRatingSoFar/speedPtsCount*.6;
+        ratingTextView.setText(String.valueOf(Math.round(overallRating)));
+
     }
 
 
